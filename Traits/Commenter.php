e9303a1ae34d0197df8fa6320d39d379214e60cd
William Verde <williamverde10@gmail.com>
2021-10-21 15:06:17 -0400

    Added: Old Services and Traits

---
+++ Traits/Commenter.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Modules\Icomments\Traits;
+
+
+use Modules\Icomments\Entities\Comment;
+
+trait Commenter
+{
+    /**
+     * Returns all comments that this user has made.
+     */
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commenter');
+    }
+
+    /**
+     * Returns only approved comments that this user has made.
+     */
+    public function approvedComments()
+    {
+        return $this->morphMany(Comment::class, 'commenter')->where('approved', true);
+    }
+}
