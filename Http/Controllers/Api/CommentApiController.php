204b59de8f13e7e58d99b00fc159d8e962c6f2f7
William Verde <williamverde10@gmail.com>
2021-10-21 15:43:35 -0400

    Added: old method create in comment api controller

--- Http/Controllers/Api/CommentApiController.php
+++ Http/Controllers/Api/CommentApiController.php
@@ -7,6 +7,8 @@ use Modules\Core\Icrud\Controllers\BaseCrudController;
 use Modules\Icomments\Entities\Comment;
 use Modules\Icomments\Repositories\CommentRepository;
 
+use Modules\Icomments\Transformers\CommentTransformer;
+
 class CommentApiController extends BaseCrudController
 {
   public $model;
@@ -17,4 +19,53 @@ class CommentApiController extends BaseCrudController
     $this->model = $model;
     $this->modelRepository = $modelRepository;
   }
+
+  /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request $request
+     * @return Response
+     */
+
+    public function create(Request $request)
+    {
+        \DB::beginTransaction();
+        try {
+            $data = $request->input('attributes') ?? [];//Get data  
+            
+            //Validate Request
+            if (isset($this->model->requestValidation['create'])) {
+              $this->validateRequestApi(new $this->model->requestValidation['create']($data));
+            }
+
+            $commentable = $data['commentable_type']::find($data["commentable_id"]);
+
+            $data['approved']=!config('comments.approval_required');
+
+            //Break if no found item
+            if (!$commentable) throw new \Exception('Commentable type not found', 404);
+
+            $data["commentable"] = $commentable;
+            $data["commenter"] = $params->user;
+            $dataEntity = $this->modelRepository->create($data);
+
+          
+            //Response
+            $response = ["data" => new CommentTransformer($dataEntity)];
+            \DB::commit(); //Commit to Data Base
+
+            $dataEntity->commentable()->associate($data['commentable']);
+            $dataEntity->commenter()->associate($data['commenter']);
+            $dataEntity->save();
+            
+        } catch (\Exception $e) {
+            \DB::rollback();//Rollback to Data Base
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+        }
+        //Return response
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+    }
+
+
 }
