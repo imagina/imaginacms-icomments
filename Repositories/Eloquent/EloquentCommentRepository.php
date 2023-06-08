f82682f0d1e7068352eb7a593f145b80f8cb846c
Juan Escalante <jcec007@gmail.com>
2023-06-08 11:40:46 -0500

    +Update: added $params to the filterQuery overrides

--- Repositories/Eloquent/EloquentCommentRepository.php
+++ Repositories/Eloquent/EloquentCommentRepository.php
@@ -26,7 +26,7 @@ class EloquentCommentRepository extends EloquentCrudRepository implements Commen
    * @param $filter
    * @return mixed
    */
-  public function filterQuery($query, $filter)
+  public function filterQuery($query, $filter, $params = false)
   {
 
     /**
